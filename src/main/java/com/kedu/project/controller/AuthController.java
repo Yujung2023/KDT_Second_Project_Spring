5c1e6db7ce6d64a5c356512c06da9a173b62151d
강범찬 <rkdqudwn1231@gmail.com>
2025-10-22 17:11:47 +0900

    통합

--- src/main/java/com/kedu/project/controller/AuthController.java
+++ src/main/java/com/kedu/project/controller/AuthController.java
@@ -1,15 +1,19 @@
 package com.kedu.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kedu.project.dto.AuthDTO;
+import com.kedu.project.dto.MemberDTO;
 import com.kedu.project.security.JwtUtil;
+import com.kedu.project.service.MemberService;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/auth")
@@ -18,40 +22,40 @@ public class AuthController {
 	@Autowired
 	private JwtUtil jwt;
 
+	@Autowired
+	private MemberService memberService;
+
 	@PostMapping
-	public ResponseEntity<String> login(@RequestBody AuthDTO dto){
+	public ResponseEntity<String> login(@RequestBody AuthDTO authDTO){
+	    System.out.println("id: " + authDTO.getId() + ": pw: " + authDTO.getPw());
+
+	    MemberDTO member = memberService.findById(authDTO.getId());
+	    String name = (member != null) ? member.getName() : "누구세요"; // null방지용
+
+	    String token = jwt.createToken(authDTO.getId(), name);
+	    return ResponseEntity.ok(token);
+	}
+
+
+
+	@GetMapping
+	public ResponseEntity<String> test(HttpServletRequest request){
+		System.out.println(request.getAttribute("loginID"));
 		
-		if(true) { // 로그인에 성공했을 경우
-			String token = jwt.createToken(dto.getId()); //토큰 생성 위치
-			 
-			return ResponseEntity.ok(token);
-		}
-		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Login Incorrect");
+		return ResponseEntity.ok("인증자 전용 데이터");
 	}
 }
 
 	
-//	@Autowired
-//	private MemberService memberService;
-//
+//	
 //	@PostMapping
-//	public ResponseEntity<String> login(@RequestBody AuthDTO authDTO){
-//	    System.out.println("id: " + authDTO.getId() + ": pw: " + authDTO.getPw());
-//
-//	    MemberDTO member = memberService.findById(authDTO.getId());
-//	    String name = (member != null) ? member.getName() : "누구세요"; // null방지용
-//
-//	    String token = jwt.createToken(authDTO.getId(), name);
-//	    return ResponseEntity.ok(token);
-//	}
-//
-//
-//
-//	@GetMapping
-//	public ResponseEntity<String> test(HttpServletRequest request){
-//		System.out.println(request.getAttribute("loginID"));
+//	public ResponseEntity<String> login(@RequestBody AuthDTO dto){
 //		
-//		return ResponseEntity.ok("인증자 전용 데이터");
+//		if(true) { // 로그인에 성공했을 경우
+//			String token = jwt.createToken(dto.getId()); //토큰 생성 위치
+//			 
+//			return ResponseEntity.ok(token);
+//		}
+//		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Login Incorrect");
 //	}
-
-
+//
