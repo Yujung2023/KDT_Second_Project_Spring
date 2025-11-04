bed155cbaadfa005226d25a1197b8c59ab8c50d0
keduit <han@naver.com>
2025-11-04 17:07:08 +0900

    파일 컨트롤러 수정

--- src/main/java/com/kedu/project/controller/FilesController.java
+++ src/main/java/com/kedu/project/controller/FilesController.java
@@ -1,10 +1,6 @@
 package com.kedu.project.controller;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,20 +11,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.google.api.gax.paging.Page;
-import com.google.cloud.storage.Blob;
-import com.google.cloud.storage.BlobId;
-import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
 import com.kedu.project.dto.FilesDTO;
-import com.kedu.project.dto.MailFileDTO;
-import com.kedu.project.service.FileService;
 import com.kedu.project.service.FilesService;
 
 @Controller
